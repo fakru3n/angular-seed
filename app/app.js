03d3809dc2b3172225d503565cb7a2078f5eba39
Syed Fakrudeen Ali <fakru3n@gmail.com>
2019-05-06 06:28:32 +0530

    Commiting for first push

--- app/app.js
+++ app/app.js
@@ -1,14 +1,57 @@
 'use strict';
-
-// Declare app level module which depends on views, and core components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/view1'});
+var app = angular.module('trelloApp', []);
+app.controller('dashboardCtrl', ['$scope', '$timeout', 'dataStore', function ($scope, $timeout, dataStore) {
+    $scope.list = [];
+    $scope.listName = '';
+    $scope.showAddListInput = false;
+    $scope.showAddListBtn = true;
+    $scope.btnName = $scope.list.length > 0 && "Add another list" || "Add a list";
+    $scope.showListInput = function () {
+        $scope.showAddListBtn = false;
+        $scope.showAddListInput = true;
+        $timeout(function () {
+            $scope.listName = ''; 
+            document.getElementById('listName').focus();
+            document.querySelector('.content').scrollTo(window.outerWidth, 0)
+        }, 0)
+    }
+    $scope.hideListInput = function () {
+        $scope.showAddListInput = false;
+        $scope.showAddListBtn = true;
+        $scope.listName = '';
+    }
+    $scope.listItems = dataStore.getData('list');
+    var addListItem = function () {
+        if (!$scope.listName) {
+            return;
+        }
+        var list = dataStore.getData('list') || [];
+        list[list.length] = { 'name': $scope.listName, 'position': list.length };
+        dataStore.setData('list', list);
+        $scope.listItems = dataStore.getData('list');
+        $scope.showListInput();
+    }
+    $scope.addList = function () {
+        addListItem();
+    }
+    $scope.addListByEnter = function(evt) {
+        if (evt.charCode === 13) {
+            addListItem();
+        }
+    }
 }]);
+
+app.factory('dataStore', function () {
+    var dataObj = {};
+    return {
+        setData: function(item, data) {
+            dataObj[item] = data;
+        },
+        getData: function(item) {
+            return dataObj[item] || null;
+        },
+        getAllData: function() {
+            return dataObj;
+        }
+    }
+})
---
+++ app/templates/list.js
@@ -0,0 +1,122 @@
+app.directive('listView', ['dataStore', '$timeout', '$q', function (dataStore, $timeout, $q) {
+    return {
+        templateUrl: 'templates/list.html',
+        scope: true,
+        compile: function (tElement, tAttributes) {
+            
+        },
+        controller: function ($scope, $element, $attrs) {
+            var _idx = $scope.$index;
+            $scope.cardName = '';
+            $scope.cards = dataStore.getData('list')[_idx]['cards'];
+
+            $scope.showComposer = false;
+            $scope.addCardBtn = true;
+
+            $scope.clickMore = function () {
+                var opEl = document.querySelector('#pop-over-' + _idx);
+                opEl.classList.add('is-shown');
+                if (window.innerWidth <= opEl.clientWidth + opEl.offsetLeft) // popup cannot load outside of document visibility
+                    opEl.style.left = "0px"
+            }
+
+            $scope.addCard = function() {
+                var waitForSomeTime = function () {
+                    var wait = $q.defer();
+                    var composers = document.querySelectorAll('.card-composer');
+                    var btns = document.querySelectorAll('.add-card-btn');
+                    if (composers) {
+                        composers.forEach(function (elem, id) {
+                            elem.classList.add("ng-hide");
+                            btns[id].classList.remove('ng-hide');
+                            if (elem.id === "card-composer-"+_idx) {
+                                elem.classList.remove("ng-hide");
+                                document.getElementById('add-card-btn-'+_idx).classList.add('ng-hide');
+                            }
+                        })
+                    }
+                    wait.resolve();
+                    return wait.promise;
+                }
+                waitForSomeTime().then(function () {
+                    $timeout(function () {
+                        $scope.cardName = '';                        
+                        document.getElementById('cardName-' + _idx).focus();
+                    }, 0);
+                })                
+            }
+
+            $scope.hideCardTitleTxtBox = function () {
+                document.getElementById('add-card-btn-' + _idx).classList.remove('ng-hide');
+                document.getElementById('card-composer-' + _idx).classList.add('ng-hide');
+                $scope.cardName = '';
+            }
+
+            var _addCardTitle = function () {
+                if (!$scope.cardName) {
+                    return;
+                }
+                var lists = dataStore.getData('list');
+                if (!lists[_idx]['cards']) 
+                    lists[_idx]['cards'] = [];
+                lists[_idx]['cards'].push({
+                    title: $scope.cardName,
+                    position: lists[_idx]['cards'].length
+                });
+                dataStore.setData('list', lists);
+                $timeout(function () {
+                    $scope.cards = dataStore.getData('list')[_idx]['cards'];
+                    $scope.addCard();
+                }, 0)                
+            }
+
+            $scope.addCardTitle = function () {
+                _addCardTitle(_idx);
+            }
+
+            $scope.addCardByKeypress = function (evt) {
+                if (evt.charCode === 13)
+                    _addCardTitle(_idx);
+            }
+
+            $scope.archiveList = function (idx) {
+                var lists = dataStore.getData('list').splice(idx, 1);
+                dataStore.setData('list', lists);
+
+            }
+
+            $(function () {
+                $('div[id^="cards-container"]').sortable({
+                    connectWith: ".sortable",
+                    receive: function (e, ui) {
+
+                    }
+
+                }).disableSelection();
+            });
+        }
+    }
+}]);
+
+/* app.directive('popOver', ['dataStore', function (dataStore) {
+    return {
+        restrict: 'C',
+        templateUrl: 'templates/popover.html',
+        scope: true,
+        controller: function ($scope, $element, $attrs) {
+            $scope.closeThis = function () {
+                $element[0].classList.remove('is-shown')
+            }
+        }
+    }
+}]); */
+
+/* app.directive('listCardDetails', ['dataStore', function (dataStore) {
+    return {
+        restrict: 'A',
+        templateUrl: 'templates/listdetails.html',
+        controller: function ($scope, $element, $attrs) {
+           
+        }
+    }
+}]); */
